6606ba08866e328a9ba18653c142534d7fe58a13
DNB <16610213+DionneNoellaBarretto@users.noreply.github.com>
2021-09-09 21:11:42 -0400

    exercise qn

--- functionsExecutionContext.js
+++ functionsExecutionContext.js
@@ -55,3 +55,8 @@ const newOutput = multiplyBy2(10);
 // global execution stack is always the first starting point!
 // in this we have multiplyby2 as the second context that pushes in and pops off after execution! 
 
+/*
+Create a function makePlans that accepts a string. This string should be a name. The function makePlans should call the function callFriend and return the result. callFriend accepts a boolean value and a string. Pass in the friendsAvailable variable and name to callFriend.
+
+Create a function callFriend that accepts a boolean value and a string. If the boolean value is true, callFriend should return the string 'Plans made with NAME this weekend'. Otherwise it should return 'Everyone is busy this weekend'.
+*/
